47079630dd7a02c671e20eb3a587d9c088df8999
Nick <petrocelli.n@husky.neu.edu>
2018-10-31 03:22:49 -0400

    Web app complete. Formatting is not good but output is there.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import app.process.Process;
 /**
- * Created by Nick on 10/30/18.
+ * Main application. Extremely simple, it just prints the output of Process to a
+ * simple webpage.
+ *
+ * Webpage is hosted at localhost:4567/parse .
+ *
+ * @author Nick Petrocelli
  */
 public class Application {
+
+  public static void main(String[] args){
+    Spark.get("/parse", new Route() {
+
+              @Override
+              public Object handle(Request request, Response response) throws Exception {
+                response.body(Process.parse());
+                return response.body();
+              }
+            });
+  }
 }
